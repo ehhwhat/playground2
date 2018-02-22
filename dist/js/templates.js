2b6383a86c555c8dba65870d6cddc707c115ced2
ehhwhat <ehhwhat@gmail.com>
2018-02-22 09:30:47 +0000

    GEN: started to try and set up handlebarsJS.

---
+++ dist/js/templates.js
@@ -0,0 +1,6 @@
+this["MyApp"] = this["MyApp"] || {};
+this["MyApp"]["templates"] = this["MyApp"]["templates"] || {};
+this["MyApp"]["templates"]["home"] = this["MyApp"]["templates"]["home"] || {};
+this["MyApp"]["templates"]["home"]["header"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<p>hello world</p>\r\n";
+},"useData":true});
